9543dc86d31a0b149a69113a27d3453a140137b4
Danail <dkondov@yandex.ru>
2018-02-11 19:10:12 +0400

    start

---
+++ src/main/java/ru/danailkondov/JRtest/JRtestApplication.java
@@ -0,0 +1,12 @@
+package ru.danailkondov.JRtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JRtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JRtestApplication.class, args);
+	}
+}
